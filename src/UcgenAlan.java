6097b335f955df34b748cb79c0e1ea733309d29b
merve82 <tarhanmerve12@gmail.com>
2024-02-03 22:14:24 +0300

    Ucgen Alan Hesaplama

--- src/UcgenAlan.java
+++ src/UcgenAlan.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class UcgenAlan {
     public static void main(String[] args) {
-        double a,b,c,alan,cevre,u;
+        double a,b,c,cevre,alan,u;
 
         Scanner input = new Scanner(System.in);
         System.out.print("1. kenari giriniz: ");
         a= input.nextInt();
         System.out.print("2. kenari giriniz: ");
-        b= input.nextInt();
+        b= input.nextDouble();
         System.out.print("3. kenari giriniz: ");
-        c= input.nextInt();
+        c= input.nextDouble();
 
         cevre= a+b+c;
         u = cevre/2;
