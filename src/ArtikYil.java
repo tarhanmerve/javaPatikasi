867bc41262be383e08bcd7934ea432bbdd6600a9
merve82 <tarhanmerve12@gmail.com>
2024-02-06 16:35:39 +0300

    Artik Yil

---
+++ src/ArtikYil.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ArtikYil {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yil Giriniz: ");
+        year = input.nextInt();
+
+        if((year%4==0 && year%100!=0) || (year%400==0) ){
+            System.out.println("" + year + " bir artık yıldır!");
+        }else{
+            System.out.println("" + year + " bir artık yıl değildir!");
+        }
+    }
+}
